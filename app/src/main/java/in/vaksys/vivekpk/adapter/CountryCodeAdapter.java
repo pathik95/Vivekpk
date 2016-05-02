ab04349205b57f5fc2dc08b392645e0b1518f125
dell980 <rakshit1993.rs@gmail.com>
2016-05-02 20:06:08 +0530

    Screen 4

--- app/src/main/java/in/vaksys/vivekpk/adapter/CountryCodeAdapter.java
+++ app/src/main/java/in/vaksys/vivekpk/adapter/CountryCodeAdapter.java
@@ -1,6 +1,7 @@
 package in.vaksys.vivekpk.adapter;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +10,11 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import in.vaksys.vivekpk.R;
+import in.vaksys.vivekpk.pojo.Country;
 
 /**
  * Created by dell980 on 5/2/2016.
@@ -18,53 +22,82 @@ import in.vaksys.vivekpk.R;
 public class CountryCodeAdapter extends BaseAdapter {
 
 
-    Context ctx = null;
-    ArrayList<String> listarray = null;
-    private LayoutInflater mInflater = null;
+    Context mContext;
+    private List<Country> worldpopulationlist = null;
+    private ArrayList<Country> arraylist;
+    LayoutInflater inflater;
 
 
-    public CountryCodeAdapter(Activity activty, ArrayList<String> list) {
-        this.ctx = activty;
-        mInflater = activty.getLayoutInflater();
-        this.listarray = list;
+    public CountryCodeAdapter(Context context, List<Country> worldpopulationlist) {
+        mContext = context;
+        this.worldpopulationlist = worldpopulationlist;
+        inflater = LayoutInflater.from(mContext);
+        this.arraylist = new ArrayList<Country>();
+        this.arraylist.addAll(worldpopulationlist);
     }
 
     @Override
     public int getCount() {
 
-        return listarray.size();
+        return worldpopulationlist.size();
     }
 
     @Override
     public Object getItem(int arg0) {
-        return null;
+        return worldpopulationlist.get(arg0);
     }
 
     @Override
     public long getItemId(int arg0) {
-        return 0;
+        return arg0;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup arg2) {
+    public View getView(final int position, View convertView, ViewGroup arg2) {
         final ViewHolder holder;
         if (convertView == null) {
             holder = new ViewHolder();
-            convertView = mInflater.inflate(R.layout.code_list_raw, null);
-
-            holder.titlename = (TextView) convertView.findViewById(R.id.textView_titllename);
+            convertView = inflater.inflate(R.layout.list_row, null);
+            // Locate the TextViews in listview_item.xml
+            holder.code = (TextView) convertView.findViewById(R.id.code);
+            holder.counryName = (TextView) convertView.findViewById(R.id.countryName);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
+        // Set the results into TextViews
+        holder.code.setText(worldpopulationlist.get(position).getCode());
+        holder.counryName.setText(worldpopulationlist.get(position).getCoutryName());
 
-        String datavalue = listarray.get(position);
-        holder.titlename.setText(datavalue);
+        // Listen for ListView Item Click
+        /*convertView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Send single item click data to SingleItemView Class
+                holder.code.setText(worldpopulationlist.get(position).getCode());
+            }
+        });*/
 
         return convertView;
     }
 
     private static class ViewHolder {
-        TextView titlename;
+        TextView code, counryName;
+    }
+
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        worldpopulationlist.clear();
+        if (charText.length() == 0) {
+            worldpopulationlist.addAll(arraylist);
+        } else {
+            for (Country wp : arraylist) {
+                if (wp.getCode().toLowerCase(Locale.getDefault()).contains(charText)) {
+                    worldpopulationlist.add(wp);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/in/vaksys/vivekpk/fragments/SignupFragment.java
+++ app/src/main/java/in/vaksys/vivekpk/fragments/SignupFragment.java
@@ -1,22 +1,50 @@
 package in.vaksys.vivekpk.fragments;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 import in.vaksys.vivekpk.R;
+import in.vaksys.vivekpk.adapter.CountryCodeAdapter;
+import in.vaksys.vivekpk.pojo.Country;
 
 /**
  * Created by dell980 on 5/2/2016.
  */
-public class SignupFragment extends Fragment {
+public class SignupFragment extends Fragment implements View.OnClickListener {
 
     EditText etCountryCode;
-    private String TitleName[]={"Sunil Gupta","Ram Chnadra"," Abhishek Tripathi","Amit Verma","Sandeep Pal","Awadhesh Diwakar","Shishir Verma","Ravi Vimal","Prabhakr Singh","Manish Srivastva","Jitendra Singh","Surendra Pal"};
+    private String[] code = {"(+36)", "(+354)", "(+91)", "(+62)", "(+62)",
+            "(+98)", "(+98)", "(+964)"};
+
+    private String[] countryName = {"Hungary", "(Iceland", "India", "Indonesia", "Iran",
+            "Iran", "Iran", "Iran"};
+
+    CountryCodeAdapter adapter;
+    ArrayList<Country> arraylist = new ArrayList<Country>();
+
+    ListView list;
+    EditText editsearch;
+    Dialog dialog;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -24,8 +52,67 @@ public class SignupFragment extends Fragment {
 
         etCountryCode = (EditText) rootView.findViewById(R.id.et_code);
 
-
+        etCountryCode.setOnClickListener(this);
 
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        ShowAlertDialogWithListview();
+    }
+
+    public void ShowAlertDialogWithListview() {
+        dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.country_list);
+
+        list = (ListView) dialog.findViewById(R.id.list);
+
+        for (int i = 0; i < code.length; i++) {
+            Country wp = new Country(code[i], countryName[i]);
+            // Binds all strings into an array
+            arraylist.add(wp);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new CountryCodeAdapter(getActivity(), arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (EditText) dialog.findViewById(R.id.et_search);
+
+        // Capture Text in EditText
+        editsearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+                String text = editsearch.getText().toString().toLowerCase(Locale.getDefault());
+                adapter.filter(text);
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1,
+                                          int arg2, int arg3) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+                                      int arg3) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                etCountryCode.setText(parent.getSelectedItem().toString());
+            }
+        });
+        dialog.show();
     }
 }
