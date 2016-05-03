79f315f34650e7e7b6671baac41eedaf932c9c5d
Harsh Dalwadi <dalwadi2@gmail.com>
2016-05-03 18:22:55 +0530

    Base Commit

--- app/src/main/java/in/vaksys/vivekpk/activities/MainActivity.java
+++ app/src/main/java/in/vaksys/vivekpk/activities/MainActivity.java
@@ -1,19 +1,14 @@
 package in.vaksys.vivekpk.activities;
 
 import android.os.Build;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.WindowManager;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import in.vaksys.vivekpk.R;
+import in.vaksys.vivekpk.adapter.ViewPagerAdapter;
 import in.vaksys.vivekpk.fragments.SigninFragment;
 import in.vaksys.vivekpk.fragments.SignupFragment;
 
@@ -35,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
-
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
@@ -48,32 +42,4 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
-    }
 }
---
+++ app/src/main/java/in/vaksys/vivekpk/fragments/BikeFragment.java
@@ -0,0 +1,29 @@
+package in.vaksys.vivekpk.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.vaksys.vivekpk.R;
+
+
+public class BikeFragment extends Fragment {
+
+    public static BikeFragment newInstance(int index) {
+        BikeFragment fragment = new BikeFragment();
+        Bundle b = new Bundle();
+        b.putInt("index", index);
+        fragment.setArguments(b);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bike, container, false);
+    }
+
+}
